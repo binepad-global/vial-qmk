9805a1f31fcc1f64a8588d51535e4035c3a8fe16
Vino Rodrigues <366673+silvinor@users.noreply.github.com>
2025-01-01 23:17:03 +1100

    Add BNR1 V3

---
+++ keyboards/binepad/bnr1/v3/config.h
@@ -0,0 +1,8 @@
+/* Copyright 2025 Binepad (@binpad) */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* "v3" has a special sub-brand to "BNR1" and requires a `info.json` override */
+/* Ignore deprecated warning at compile */
+#define PRODUCT "Shuttle Cue Lite"

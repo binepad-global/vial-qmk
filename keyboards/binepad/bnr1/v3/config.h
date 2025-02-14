504deb1da7e98432e73588f4439796a391dbff67
silvinor <366673+silvinor@users.noreply.github.com>
2025-02-14 17:55:38 +1100

    adjust encoder resolution

--- keyboards/binepad/bnr1/v3/config.h
+++
@@ -1,8 +0,0 @@
-/* Copyright 2025 Binepad (@binpad) */
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-/* "v3" has a special sub-brand to "BNR1" and requires a `info.json` override */
-/* Ignore deprecated warning at compile */
-#define PRODUCT "Shuttle Cue Lite"
